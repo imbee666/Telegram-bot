dd019f347332b4eab68c1ba67b9f76709134576b
imbee666 <swamimanikanta641@gmail.com>
2026-05-23 18:50:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ import os
 # =========================
 # TOKEN
 # =========================
-TOKEN = os.getenv("8993909189:AAEh6viwCqmTuqKFO6lm1Q4K9jG3uThHErk")
+TOKEN = os.getenv("TOKEN")
 
 
 # =========================
@@ -26,99 +26,99 @@ TOKEN = os.getenv("8993909189:AAEh6viwCqmTuqKFO6lm1Q4K9jG3uThHErk")
 DATA = {
     "show_images": {
         "title": "𝐔𝐍𝐂𝐔𝐓 𝐃𝐄𝐒𝐈 𝐖𝐄𝐁𝐒𝐄𝐑𝐈𝐄𝐒",
-        "image": r"D:\demo.jpg"
+        "image": r"images\demo.jpg"
     },
     "animal": {
         "title": "𝐌𝐄𝐍 𝐀𝐍𝐃 𝐀𝐍𝐈𝐌𝐀𝐋𝐒",
-        "image": r"D:\demo2.jpg"
+        "image": r"images\demo2.jpg"
     },
     "model": {
         "title": "𝐌𝐎𝐃𝐄𝐋",
-        "image": r"D:\demo4.jpg"
+        "image": r"images\demo4.jpg"
     },
     "onlyfan": {
         "title": "𝐎𝐍𝐋𝐘 𝐅𝐀𝐍𝐒",
-        "image": r"D:\demo5.jpg"
+        "image": r"images\demo5.jpg"
     },
     "omegal": {
         "title": "𝐎𝐌𝐄𝐆𝐀𝐋 𝐋𝐄𝐀𝐊𝐒",
-        "image": r"D:\demo6.jpg"
+        "image": r"images\demo6.jpg"
     },
     "tiktok": {
         "title": "𝐓𝐈𝐊 𝐓𝐎𝐊 𝐒𝐇𝐎𝐑𝐓𝐒",
-        "image": r"D:\demo7.jpg"
+        "image": r"images\demo7.jpg"
     },
     "indian": {
         "title": "𝐈𝐍𝐃𝐈𝐀𝐍 𝐋𝐄𝐒𝐁𝐈𝐀𝐍",
-        "image": r"D:\demo8.jpg"
+        "image": r"images\demo8.jpg"
     },
     "foreign": {
         "title": "𝐅𝐎𝐑𝐄𝐈𝐆𝐍 𝐋𝐄𝐒𝐁𝐈𝐀𝐍",
-        "image": r"D:\demo9.jpg"
+        "image": r"images\demo9.jpg"
     },
     "foreignshe": {
         "title": "𝐅𝐎𝐑𝐄𝐈𝐆𝐍 𝐒𝐇𝐄𝐌𝐀𝐋𝐄",
-        "image": r"D:\demo11.jpg"
+        "image": r"images\demo11.jpg"
     },
     "indianshe": {
         "title": "𝐈𝐍𝐃𝐈𝐀𝐍 𝐒𝐇𝐄𝐌𝐀𝐋𝐄",
-        "image": r"D:\demo10.jpg"
+        "image": r"images\demo10.jpg"
     },
     "gay": {
         "title": "𝐈𝐍𝐃𝐈𝐀𝐍 & 𝐅𝐎𝐑𝐄𝐈𝐆𝐍 𝐆@𝐘",
-        "image": r"D:\demo12.jpg"
+        "image": r"images\demo12.jpg"
     },
     "russian": {
         "title": "𝐑𝐔𝐒𝐒𝐈𝐀𝐍 𝐓𝐄𝐄𝐍𝐒 𝐋𝐄𝐀𝐊",
-        "image": r"D:\demo13.jpg"
+        "image": r"images\demo13.jpg"
     },
     "school": {
         "title": "𝐈𝐍𝐃𝐈𝐀𝐍 𝐒𝐂𝐇𝐎𝐎𝐋 𝐆𝐈𝐑𝐋",
-        "image": r"D:\demo14.jpg"
+        "image": r"images\demo14.jpg"
     },
     "college": {
         "title": "𝐂𝐎𝐋𝐋𝐄𝐆𝐄 𝐆𝐈𝐑𝐋𝐒",
-        "image": r"D:\demo15.jpg"
+        "image": r"images\demo15.jpg"
     },
     "teens": {
         "title": "𝐈𝐍𝐃𝐈𝐀𝐍 𝐓𝐄𝐄𝐍𝐒",
-        "image": r"D:\demo16.jpg"
+        "image": r"images\demo16.jpg"
     },
     "oyo": {
         "title": "𝐎𝐘𝐎 𝐇𝐈𝐃𝐃𝐄𝐍 𝐂𝐀𝐌",
-        "image": r"D:\demo17.jpg"
+        "image": r"images\demo17.jpg"
     },
     "snapchat": {
         "title": "𝐒𝐍𝐀𝐏𝐂𝐇𝐀𝐓/𝐈𝐍𝐒𝐓𝐀 𝐋𝐄𝐀𝐊𝐒",
-        "image": r"D:\demo18.jpg"
+        "image": r"images\demo18.jpg"
     },
     "mms": {
         "title": "𝐃𝐀𝐈𝐋𝐘 𝐃𝐄𝐒𝐈 𝐌𝐌𝐒",
-        "image": r"D:\demo19.jpg"
+        "image": r"images\demo19.jpg"
     },
     "bhabhi": {
         "title": "𝐈𝐍𝐃𝐈𝐀𝐍 𝐁𝐇𝐀𝐁𝐇𝐈",
-        "image": r"D:\demo20.jpg"
+        "image": r"images\demo20.jpg"
     },
     "room": {
         "title": "𝐌𝐎𝐌 𝐑𝐎𝐎𝐌 𝐋𝐄𝐀𝐊𝐒",
-        "image": r"D:\demo21.jpg"
+        "image": r"images\demo21.jpg"
     },
     "touch": {
         "title": "𝐈𝐍𝐂𝐄𝐒𝐓 𝐅𝐀𝐌𝐈𝐋𝐘 𝐓𝐎𝐔𝐂𝐇",
-        "image": r"D:\demo22.jpg"
+        "image": r"images\demo22.jpg"
     },
     "realmom": {
         "title": "𝐑𝐄𝐀𝐋 𝐌𝐎𝐌 - 𝐒𝐎𝐍",
-        "image": r"D:\demo23.jpg"
+        "image": r"images\demo23.jpg"
     },
     "realbro": {
         "title": "𝐑𝐄𝐀𝐋 𝐁𝐑𝐎 - 𝐒𝐈𝐒",
-        "image": r"D:\demo24.jpg"
+        "image": r"images\demo24.jpg"
     },
     "realdad": {
         "title": "𝐑𝐄𝐀𝐋 𝐃𝐀𝐃 - 𝐃𝐀𝐔𝐆𝐇𝐓𝐄𝐑",
-        "image": r"D:\demo25.jpg"
+        "image": r"images\demo25.jpg"
     }
 }
 
